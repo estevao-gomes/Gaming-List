c579134e49618a16ca05c2daa1ab56c2b70e78e2
Estevão Gomes <84734546+estevao-gomes@users.noreply.github.com>
2022-02-10 20:29:14 -0300

    Added main files.
    Added app and components.

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from "./components/Header.js"
+import Bakcground from "./components/Background.js"
+import Footer from "./components/Footer.js"
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Bakcground/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
